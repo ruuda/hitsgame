719ff4b5ea7e638f9e0e0dbc86a9bd366f90ea86
Ruud van Asseldonk <dev@veniogames.com>
2023-12-09 21:25:28 +0100

    Line-wrap long titles/artists

--- mkhitsgame.py
+++ mkhitsgame.py
@@ -13,7 +13,7 @@ import tomllib
 import qrcode
 from qrcode.image.svg import SvgPathImage
 
-from typing import Dict, List, Literal, NamedTuple, Tuple
+from typing import Dict, Iterable, List, Literal, NamedTuple, Tuple
 
 def metaflac_get_tags(fname: str) -> Tuple[str, Dict[str, str]]:
     """
@@ -95,6 +95,50 @@ class Config(NamedTuple):
         with open(fname, "rb") as f:
             toml = tomllib.load(f)
             return Config(**toml)
+
+
+def line_break_text(s: str) -> List[str]:
+    """
+    Line break the artist and title so they (hopefully) fit on a card. This is a
+    hack based on string lengths, but it's good enough for most cases.
+    """
+    if len(s) < 28:
+        return [s]
+
+    words = s.split(" ")
+    char_count = sum(len(word) for word in words)
+
+    # The starting situation is everything on the first line. We'll try out
+    # every possible line break and pick the one with the most even distribution
+    # (by characters in the string, not true text width).
+    top, bot = " ".join(words), ""
+    diff = char_count
+
+    # Try line-breaking between every word.
+    for i in range(1, len(words) - 1):
+        w1, w2 = words[:i], words[i:]
+        t, b = " ".join(w1), " ".join(w2)
+        d = abs(len(t) - len(b))
+        if d < diff:
+            top, bot, diff = t, b, d
+
+    return [top, bot]
+
+
+def render_text_svg(x_mm: float, y_mm: float, s: str, class_: str) -> Iterable[str]:
+    """
+    Render the artist or title, broken across lines if needed.
+    """
+    lines = line_break_text(s)
+    line_height_mm = 7
+    h_mm = line_height_mm * len(lines)
+
+    for i, line in enumerate(lines):
+        dy_mm = line_height_mm * (1 + i) - h_mm / 2
+        yield (
+            f'<text x="{x_mm}" y="{y_mm + dy_mm}" text-anchor="middle" '
+            f'class="{class_}">{html.escape(line)}</text>'
+        )
 
 
 class Table(NamedTuple):
@@ -197,20 +241,14 @@ class Table(NamedTuple):
                 iy = i // self.width
                 x_mm = hmargin_mm + (ix + 0.5) * side_mm
                 y_mm = vmargin_mm + (iy + 0.5) * side_mm
-                title = html.escape(track.title)
-                artist = html.escape(track.artist)
                 parts.append(
                     f'<text x="{x_mm}" y="{y_mm + 7}" text-anchor="middle" '
                     f'class="year">{track.year}</text>'
                 )
-                parts.append(
-                    f'<text x="{x_mm}" y="{y_mm - 14}" text-anchor="middle" '
-                    f'class="artist">{artist}</text>'
-                )
-                parts.append(
-                    f'<text x="{x_mm}" y="{y_mm + 22}" text-anchor="middle" '
-                    f'class="title">{title}</text>'
-                )
+                for part in render_text_svg(x_mm, y_mm - 19, track.artist, "artist"):
+                    parts.append(part)
+                for part in render_text_svg(x_mm, y_mm + 18, track.title, "title"):
+                    parts.append(part)
 
         parts.append("</svg>")
 
